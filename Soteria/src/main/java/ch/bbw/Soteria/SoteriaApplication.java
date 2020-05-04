02eff9c13052268aac5226953f77067f95df4754
Duarte <Duarte.amarelo@hotmail.com>
2020-05-04 16:28:48 +0200

    Creation of Project shell and extending gitignore for Eclipse and IntelliJ

---
+++ Soteria/src/main/java/ch/bbw/Soteria/SoteriaApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.Soteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoteriaApplication.class, args);
+	}
+
+}
---
+++ Soteria/src/test/java/ch/bbw/Soteria/SoteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.Soteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
